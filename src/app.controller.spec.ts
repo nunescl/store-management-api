accf56eed19149ad37f9391f62c8fcc47f9fa5d9
Ana Nunes <anaclaranunes@gmail.com>
2022-09-12 16:06:58 -0300

    init commit

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -17,6 +17,7 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+      expect(1).toBe(1);
     });
   });
 });
