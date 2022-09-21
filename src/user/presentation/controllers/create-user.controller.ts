a7f36d7b88ebc337e81985bbfaa872bc96ada650
Ana Mendes <anaclaranunes@gmail.com>
2022-09-21 13:56:01 -0300

    feat: create-user-controller creation

---
+++ src/user/presentation/controllers/create-user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserService } from 'src/user/usecases/create-user.usecase';
+import { CreateUserDto } from '../dtos/create-user.dto';
+
+@Controller('createuser')
+export class CreateUserController {
+  constructor(private createUserService: CreateUserService) {}
+
+  @Post('/signup')
+  createUser(@Body() createUserDto: CreateUserDto): Promise<void> {
+    return this.createUserService.createUser(createUserDto);
+  }
+}
