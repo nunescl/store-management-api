df651fd30910b6e47b7af7872b35583c8a7a0369
Ana Mendes <anaclaranunes@gmail.com>
2022-09-21 14:04:19 -0300

    feat: refactored code for dependency inversion

--- src/user/infra/user.repository.ts
+++ src/user/infra/user.repository.ts
@@ -6,11 +6,16 @@ import { SaveUser } from './user.repository.types';
 @Injectable()
 export class UsersRepository {
   private repo: Repository<UserEntity>;
+  constructor(private dataSource: DataSource) {
+    this.repo = dataSource.getRepository(UserEntity);
+  }
+
   public async createUser(user: SaveUser) {
     await this.repo.save(user);
   }
 
-  constructor(private dataSource: DataSource) {
-    this.repo = dataSource.getRepository(UserEntity);
+  public async findUser(username: string) {
+    const user = await this.repo.findOne({ where: { username } });
+    return user;
   }
 }
